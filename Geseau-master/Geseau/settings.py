7dbd2fde92035b11ff3be8983e9ec34dbcfe498f
maigaamidou722 <maigaamidou722@gmail.com>
2024-08-08 22:44:41 +0000

    3

--- Geseau-master/Geseau/settings.py
+++ Geseau-master/Geseau/settings.py
@@ -86,6 +86,21 @@ DATABASES = {
     }
 }
 
+
+#AWS S3 BUCKET = Stockage
+
+AWS_ACCES_KEY_ID = 'AKIAYQZLJDUIRVTDQFSU'
+AWS_SECRET_ACCES_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'deploiedatabase'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
