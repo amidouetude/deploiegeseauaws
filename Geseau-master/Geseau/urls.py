57dd2028f77e60f43b46af6dcd6741b718e509c0
maigaamidou722 <maigaamidou722@gmail.com>
2024-11-11 15:28:20 +0000

    correction

--- Geseau-master/Geseau/urls.py
+++ Geseau-master/Geseau/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('conso.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
